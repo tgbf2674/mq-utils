fb45106120ec743c6fdcf76199c3c72ed68112be
maqing <136518041@qq.com>
2022-09-01 03:20:47 +0800

    resolve gulpfile.js error

--- gulpfile.ts
+++
@@ -1,9 +0,0 @@
-// @ts-ignore
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const gulp = require('gulp')
-gulp.task('copy_resource', () => {
-  return gulp.src('./func/*')
-    .pipe(gulp.dest('./'))
-})
-
-gulp.task('build', gulp.series('copy_resource'))
